90a0608205f85cd1290a32a8adb0820428e2798e
Dustin Johns <dustin.johns1@gmail.com>
2022-05-10 18:12:02 -0500

    removing some more devexpress stuff, upgrading .net version

--- GuitarBroWindows.Designer.cs
+++ GuitarBroWindows.Designer.cs
@@ -75,27 +75,30 @@
             // scaleLabel
             // 
             this.scaleLabel.AutoSize = true;
-            this.scaleLabel.Location = new System.Drawing.Point(12, 51);
+            this.scaleLabel.Location = new System.Drawing.Point(16, 63);
+            this.scaleLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.scaleLabel.Name = "scaleLabel";
-            this.scaleLabel.Size = new System.Drawing.Size(34, 13);
+            this.scaleLabel.Size = new System.Drawing.Size(42, 16);
             this.scaleLabel.TabIndex = 19;
             this.scaleLabel.Text = "Scale";
             // 
             // guitarTuningLabel
             // 
             this.guitarTuningLabel.AutoSize = true;
-            this.guitarTuningLabel.Location = new System.Drawing.Point(153, 9);
+            this.guitarTuningLabel.Location = new System.Drawing.Point(204, 11);
+            this.guitarTuningLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.guitarTuningLabel.Name = "guitarTuningLabel";
-            this.guitarTuningLabel.Size = new System.Drawing.Size(40, 13);
+            this.guitarTuningLabel.Size = new System.Drawing.Size(48, 16);
             this.guitarTuningLabel.TabIndex = 17;
             this.guitarTuningLabel.Text = "Tuning";
             // 
             // keyLabel
             // 
             this.keyLabel.AutoSize = true;
-            this.keyLabel.Location = new System.Drawing.Point(12, 9);
+            this.keyLabel.Location = new System.Drawing.Point(16, 11);
+            this.keyLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.keyLabel.Name = "keyLabel";
-            this.keyLabel.Size = new System.Drawing.Size(25, 13);
+            this.keyLabel.Size = new System.Drawing.Size(30, 16);
             this.keyLabel.TabIndex = 16;
             this.keyLabel.Text = "Key";
             // 
@@ -115,9 +118,10 @@
             "F#",
             "G",
             "G#"});
-            this.keyCombo.Location = new System.Drawing.Point(15, 25);
+            this.keyCombo.Location = new System.Drawing.Point(20, 31);
+            this.keyCombo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.keyCombo.Name = "keyCombo";
-            this.keyCombo.Size = new System.Drawing.Size(41, 21);
+            this.keyCombo.Size = new System.Drawing.Size(53, 24);
             this.keyCombo.TabIndex = 20;
             this.keyCombo.SelectedIndexChanged += new System.EventHandler(this.KeyCombo_SelectedIndexChanged);
             // 
@@ -137,9 +141,10 @@
             "F#",
             "G",
             "G#"});
-            this.guitarKeyCombo.Location = new System.Drawing.Point(156, 25);
+            this.guitarKeyCombo.Location = new System.Drawing.Point(208, 31);
+            this.guitarKeyCombo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guitarKeyCombo.Name = "guitarKeyCombo";
-            this.guitarKeyCombo.Size = new System.Drawing.Size(37, 21);
+            this.guitarKeyCombo.Size = new System.Drawing.Size(48, 24);
             this.guitarKeyCombo.TabIndex = 21;
             this.guitarKeyCombo.SelectedIndexChanged += new System.EventHandler(this.GuitarKeyCombo_SelectedIndexChanged);
             // 
@@ -151,9 +156,10 @@
             "Minor",
             "Pentatonic",
             "Blues"});
-            this.scaleCombo.Location = new System.Drawing.Point(15, 67);
+            this.scaleCombo.Location = new System.Drawing.Point(20, 82);
+            this.scaleCombo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.scaleCombo.Name = "scaleCombo";
-            this.scaleCombo.Size = new System.Drawing.Size(121, 21);
+            this.scaleCombo.Size = new System.Drawing.Size(160, 24);
             this.scaleCombo.TabIndex = 22;
             this.scaleCombo.SelectedIndexChanged += new System.EventHandler(this.ScaleCombo_SelectedIndexChanged);
             // 
@@ -164,53 +170,59 @@
             "Standard",
             "Drop",
             "Open"});
-            this.tuningTypeCombo.Location = new System.Drawing.Point(199, 25);
+            this.tuningTypeCombo.Location = new System.Drawing.Point(265, 31);
+            this.tuningTypeCombo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tuningTypeCombo.Name = "tuningTypeCombo";
-            this.tuningTypeCombo.Size = new System.Drawing.Size(74, 21);
+            this.tuningTypeCombo.Size = new System.Drawing.Size(97, 24);
             this.tuningTypeCombo.TabIndex = 23;
             this.tuningTypeCombo.SelectedIndexChanged += new System.EventHandler(this.TuningTypeCombo_SelectedIndexChanged);
             // 
             // keyDisplayLabel
             // 
             this.keyDisplayLabel.AutoSize = true;
-            this.keyDisplayLabel.Location = new System.Drawing.Point(492, 9);
+            this.keyDisplayLabel.Location = new System.Drawing.Point(656, 11);
+            this.keyDisplayLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.keyDisplayLabel.Name = "keyDisplayLabel";
-            this.keyDisplayLabel.Size = new System.Drawing.Size(43, 13);
+            this.keyDisplayLabel.Size = new System.Drawing.Size(52, 16);
             this.keyDisplayLabel.TabIndex = 24;
             this.keyDisplayLabel.Text = "E Minor";
             // 
             // guitarStringTuningDisplayLabel
             // 
             this.guitarStringTuningDisplayLabel.AutoSize = true;
-            this.guitarStringTuningDisplayLabel.Location = new System.Drawing.Point(492, 76);
+            this.guitarStringTuningDisplayLabel.Location = new System.Drawing.Point(656, 94);
+            this.guitarStringTuningDisplayLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.guitarStringTuningDisplayLabel.Name = "guitarStringTuningDisplayLabel";
-            this.guitarStringTuningDisplayLabel.Size = new System.Drawing.Size(66, 13);
+            this.guitarStringTuningDisplayLabel.Size = new System.Drawing.Size(78, 16);
             this.guitarStringTuningDisplayLabel.TabIndex = 26;
             this.guitarStringTuningDisplayLabel.Text = "E A D G B E";
             // 
             // guitarTuningDisplayLabel
             // 
             this.guitarTuningDisplayLabel.AutoSize = true;
-            this.guitarTuningDisplayLabel.Location = new System.Drawing.Point(492, 56);
+            this.guitarTuningDisplayLabel.Location = new System.Drawing.Point(656, 69);
+            this.guitarTuningDisplayLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.guitarTuningDisplayLabel.Name = "guitarTuningDisplayLabel";
-            this.guitarTuningDisplayLabel.Size = new System.Drawing.Size(88, 13);
+            this.guitarTuningDisplayLabel.Size = new System.Drawing.Size(105, 16);
             this.guitarTuningDisplayLabel.TabIndex = 25;
             this.guitarTuningDisplayLabel.Text = "Tune Guitar To...";
             // 
             // scalePatternDisplayLabel
             // 
             this.scalePatternDisplayLabel.AutoSize = true;
-            this.scalePatternDisplayLabel.Location = new System.Drawing.Point(492, 31);
+            this.scalePatternDisplayLabel.Location = new System.Drawing.Point(656, 38);
+            this.scalePatternDisplayLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.scalePatternDisplayLabel.Name = "scalePatternDisplayLabel";
-            this.scalePatternDisplayLabel.Size = new System.Drawing.Size(82, 13);
+            this.scalePatternDisplayLabel.Size = new System.Drawing.Size(96, 16);
             this.scalePatternDisplayLabel.TabIndex = 27;
             this.scalePatternDisplayLabel.Text = "E F# G A B C D";
             // 
             // csvButton
             // 
-            this.csvButton.Location = new System.Drawing.Point(618, 71);
+            this.csvButton.Location = new System.Drawing.Point(824, 87);
+            this.csvButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.csvButton.Name = "csvButton";
-            this.csvButton.Size = new System.Drawing.Size(75, 23);
+            this.csvButton.Size = new System.Drawing.Size(100, 28);
             this.csvButton.TabIndex = 28;
             this.csvButton.Text = "save to CSV";
             this.csvButton.UseVisualStyleBackColor = true;
@@ -249,18 +261,20 @@
             this.fret22,
             this.fret23,
             this.fret24});
-            this.fretBoardGrid.Location = new System.Drawing.Point(15, 100);
+            this.fretBoardGrid.Location = new System.Drawing.Point(19, 130);
+            this.fretBoardGrid.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.fretBoardGrid.Name = "fretBoardGrid";
             this.fretBoardGrid.ReadOnly = true;
             this.fretBoardGrid.RowHeadersWidth = 50;
             this.fretBoardGrid.ScrollBars = System.Windows.Forms.ScrollBars.None;
             this.fretBoardGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
-            this.fretBoardGrid.Size = new System.Drawing.Size(678, 154);
+            this.fretBoardGrid.Size = new System.Drawing.Size(904, 190);
             this.fretBoardGrid.TabIndex = 29;
             // 
             // fret0
             // 
             this.fret0.HeaderText = "0";
+            this.fret0.MinimumWidth = 6;
             this.fret0.Name = "fret0";
             this.fret0.ReadOnly = true;
             this.fret0.Width = 25;
@@ -268,6 +282,7 @@
             // fret1
             // 
             this.fret1.HeaderText = "1";
+            this.fret1.MinimumWidth = 6;
             this.fret1.Name = "fret1";
             this.fret1.ReadOnly = true;
             this.fret1.Width = 25;
@@ -275,6 +290,7 @@
             // fret2
             // 
             this.fret2.HeaderText = "2";
+            this.fret2.MinimumWidth = 6;
             this.fret2.Name = "fret2";
             this.fret2.ReadOnly = true;
             this.fret2.Width = 25;
@@ -282,6 +298,7 @@
             // fret3
             // 
             this.fret3.HeaderText = "3";
+            this.fret3.MinimumWidth = 6;
             this.fret3.Name = "fret3";
             this.fret3.ReadOnly = true;
             this.fret3.Width = 25;
@@ -289,6 +306,7 @@
             // fret4
             // 
             this.fret4.HeaderText = "4";
+            this.fret4.MinimumWidth = 6;
             this.fret4.Name = "fret4";
             this.fret4.ReadOnly = true;
             this.fret4.Width = 25;
@@ -296,6 +314,7 @@
             // fret5
             // 
             this.fret5.HeaderText = "5";
+            this.fret5.MinimumWidth = 6;
             this.fret5.Name = "fret5";
             this.fret5.ReadOnly = true;
             this.fret5.Width = 25;
@@ -303,6 +322,7 @@
             // fret6
             // 
             this.fret6.HeaderText = "6";
+            this.fret6.MinimumWidth = 6;
             this.fret6.Name = "fret6";
             this.fret6.ReadOnly = true;
             this.fret6.Width = 25;
@@ -310,6 +330,7 @@
             // fret7
             // 
             this.fret7.HeaderText = "7";
+            this.fret7.MinimumWidth = 6;
             this.fret7.Name = "fret7";
             this.fret7.ReadOnly = true;
             this.fret7.Width = 25;
@@ -317,6 +338,7 @@
             // fret8
             // 
             this.fret8.HeaderText = "8";
+            this.fret8.MinimumWidth = 6;
             this.fret8.Name = "fret8";
             this.fret8.ReadOnly = true;
             this.fret8.Width = 25;
@@ -324,6 +346,7 @@
             // fret9
             // 
             this.fret9.HeaderText = "9";
+            this.fret9.MinimumWidth = 6;
             this.fret9.Name = "fret9";
             this.fret9.ReadOnly = true;
             this.fret9.Width = 25;
@@ -331,6 +354,7 @@
             // fret10
             // 
             this.fret10.HeaderText = "10";
+            this.fret10.MinimumWidth = 6;
             this.fret10.Name = "fret10";
             this.fret10.ReadOnly = true;
             this.fret10.Width = 25;
@@ -338,6 +362,7 @@
             // fret11
             // 
             this.fret11.HeaderText = "11";
+            this.fret11.MinimumWidth = 6;
             this.fret11.Name = "fret11";
             this.fret11.ReadOnly = true;
             this.fret11.Width = 25;
@@ -345,6 +370,7 @@
             // fret12
             // 
             this.fret12.HeaderText = "12";
+            this.fret12.MinimumWidth = 6;
             this.fret12.Name = "fret12";
             this.fret12.ReadOnly = true;
             this.fret12.Width = 25;
@@ -352,6 +378,7 @@
             // fret13
             // 
             this.fret13.HeaderText = "13";
+            this.fret13.MinimumWidth = 6;
             this.fret13.Name = "fret13";
             this.fret13.ReadOnly = true;
             this.fret13.Width = 25;
@@ -359,6 +386,7 @@
             // fret14
             // 
             this.fret14.HeaderText = "14";
+            this.fret14.MinimumWidth = 6;
             this.fret14.Name = "fret14";
             this.fret14.ReadOnly = true;
             this.fret14.Width = 25;
@@ -366,6 +394,7 @@
             // fret15
             // 
             this.fret15.HeaderText = "15";
+            this.fret15.MinimumWidth = 6;
             this.fret15.Name = "fret15";
             this.fret15.ReadOnly = true;
             this.fret15.Width = 25;
@@ -373,6 +402,7 @@
             // fret16
             // 
             this.fret16.HeaderText = "16";
+            this.fret16.MinimumWidth = 6;
             this.fret16.Name = "fret16";
             this.fret16.ReadOnly = true;
             this.fret16.Width = 25;
@@ -380,6 +410,7 @@
             // fret17
             // 
             this.fret17.HeaderText = "17";
+            this.fret17.MinimumWidth = 6;
             this.fret17.Name = "fret17";
             this.fret17.ReadOnly = true;
             this.fret17.Width = 25;
@@ -387,6 +418,7 @@
             // fret18
             // 
             this.fret18.HeaderText = "18";
+            this.fret18.MinimumWidth = 6;
             this.fret18.Name = "fret18";
             this.fret18.ReadOnly = true;
             this.fret18.Width = 25;
@@ -394,6 +426,7 @@
             // fret19
             // 
             this.fret19.HeaderText = "19";
+            this.fret19.MinimumWidth = 6;
             this.fret19.Name = "fret19";
             this.fret19.ReadOnly = true;
             this.fret19.Width = 25;
@@ -401,6 +434,7 @@
             // fret20
             // 
             this.fret20.HeaderText = "20";
+            this.fret20.MinimumWidth = 6;
             this.fret20.Name = "fret20";
             this.fret20.ReadOnly = true;
             this.fret20.Width = 25;
@@ -408,6 +442,7 @@
             // fret21
             // 
             this.fret21.HeaderText = "21";
+            this.fret21.MinimumWidth = 6;
             this.fret21.Name = "fret21";
             this.fret21.ReadOnly = true;
             this.fret21.Width = 25;
@@ -415,6 +450,7 @@
             // fret22
             // 
             this.fret22.HeaderText = "22";
+            this.fret22.MinimumWidth = 6;
             this.fret22.Name = "fret22";
             this.fret22.ReadOnly = true;
             this.fret22.Width = 25;
@@ -422,6 +458,7 @@
             // fret23
             // 
             this.fret23.HeaderText = "23";
+            this.fret23.MinimumWidth = 6;
             this.fret23.Name = "fret23";
             this.fret23.ReadOnly = true;
             this.fret23.Width = 25;
@@ -429,6 +466,7 @@
             // fret24
             // 
             this.fret24.HeaderText = "24";
+            this.fret24.MinimumWidth = 6;
             this.fret24.Name = "fret24";
             this.fret24.ReadOnly = true;
             this.fret24.Width = 25;
@@ -439,18 +477,20 @@
             this.majorMinorCombo.Items.AddRange(new object[] {
             "Major",
             "Minor"});
-            this.majorMinorCombo.Location = new System.Drawing.Point(62, 25);
+            this.majorMinorCombo.Location = new System.Drawing.Point(83, 31);
+            this.majorMinorCombo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.majorMinorCombo.Name = "majorMinorCombo";
-            this.majorMinorCombo.Size = new System.Drawing.Size(74, 21);
+            this.majorMinorCombo.Size = new System.Drawing.Size(97, 24);
             this.majorMinorCombo.TabIndex = 30;
             this.majorMinorCombo.SelectedIndexChanged += new System.EventHandler(this.MajorMinorCombo_SelectedIndexChanged);
             // 
             // fretboardDisplayLabel
             // 
             this.fretboardDisplayLabel.AutoSize = true;
-            this.fretboardDisplayLabel.Location = new System.Drawing.Point(153, 51);
+            this.fretboardDisplayLabel.Location = new System.Drawing.Point(204, 63);
+            this.fretboardDisplayLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.fretboardDisplayLabel.Name = "fretboardDisplayLabel";
-            this.fretboardDisplayLabel.Size = new System.Drawing.Size(40, 13);
+            this.fretboardDisplayLabel.Size = new System.Drawing.Size(48, 16);
             this.fretboardDisplayLabel.TabIndex = 31;
             this.fretboardDisplayLabel.Text = "Tuning";
             // 
@@ -461,17 +501,18 @@
             "Notes",
             "Frets",
             "Dots"});
-            this.fretboardDisplayCombo.Location = new System.Drawing.Point(156, 67);
+            this.fretboardDisplayCombo.Location = new System.Drawing.Point(208, 82);
+            this.fretboardDisplayCombo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.fretboardDisplayCombo.Name = "fretboardDisplayCombo";
-            this.fretboardDisplayCombo.Size = new System.Drawing.Size(121, 21);
+            this.fretboardDisplayCombo.Size = new System.Drawing.Size(160, 24);
             this.fretboardDisplayCombo.TabIndex = 32;
             this.fretboardDisplayCombo.SelectedIndexChanged += new System.EventHandler(this.FretboardDisplayCombo_SelectedIndexChanged);
             // 
             // GuitarBroWindows
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(707, 316);
+            this.ClientSize = new System.Drawing.Size(943, 333);
             this.Controls.Add(this.fretboardDisplayCombo);
             this.Controls.Add(this.fretboardDisplayLabel);
             this.Controls.Add(this.majorMinorCombo);
@@ -488,6 +529,7 @@
             this.Controls.Add(this.scaleLabel);
             this.Controls.Add(this.guitarTuningLabel);
             this.Controls.Add(this.keyLabel);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "GuitarBroWindows";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.LoadGuitarBro);
